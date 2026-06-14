91e9eefb86d519688cd14cf9e0904b222e78aca9
hudha081 <hudhafathima081@gmail.com>
2026-06-14 13:25:32 +0530

    changes footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="mt-20 border-t border-white/10 py-6 text-center text-sm text-muted-foreground">
       <p>
-        Built with <span className="text-red-500">❤</span> by{" "}
+        Built with <span className="text-red-500">♡</span> by{" "}
         <span className="font-semibold text-white">Huda</span>
       </p>
 
